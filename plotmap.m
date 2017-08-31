f6129ecc37210bd2a9823883e2bc1d0d57fd2016
Ben Sanderson <bsander@yslogin1.(none)>
2017-08-31 14:56:49 -0600

    cleaning up...

--- plotmap.m
+++ plotmap.m
@@ -1,5 +1,5 @@
-function plotmap(lats,lon,aa,bb,cmap,filename)
-figure(3)
+function plotmap(lats,lon,aa,bb,cmap,filename,n)
+figure(n)
 clf
 ax = usamap('conus');
 states = shaperead('usastatelo', 'UseGeoCoords', true,...
@@ -7,20 +7,20 @@ states = shaperead('usastatelo', 'UseGeoCoords', true,...
   {@(name) ~any(strcmp(name,{'Alaska','Hawaii'})), 'Name'});
 faceColors = makesymbolspec('Polygon',...
     {'INDEX', [1 numel(states)], 'FaceColor','none'}); %NOTE - colors are random
- geoshow(lats(bb),lon(aa),cmap, 'DisplayType', 'texturemap')
+ geoshow(lats(bb),lon(aa),cmap, 'DisplayType', 'texturemap');
 
 hold on
 geoshow(ax, states, 'DisplayType', 'polygon', ...
-   'SymbolSpec', faceColors)
+   'SymbolSpec', faceColors);
  load coast
-geoshow(flipud(lat),flipud(long),'DisplayType','polygon','FaceColor','white')
-colormap('jet')
+geoshow(flipud(lat),flipud(long),'DisplayType','polygon','FaceColor','white');
+colormap('jet');
 
 
 
 set(gcf, 'PaperPosition', [0 0 6 5]);
 set(gcf, 'PaperSize', [6 5]);
 
-print(gcf,'-dpng','-r600','-painters',filename)
+print(gcf,'-dpng','-r600','-painters',filename);
 
 
